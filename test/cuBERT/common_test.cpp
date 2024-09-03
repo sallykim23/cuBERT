c3dfbb34d34661671d1c93ec76f2b8452e03f090
Saeyeon Kim <skim8@wave8-login3.wave.scu.edu>
2024-09-03 16:28:44 -0700

    mpi-function calls

--- test/cuBERT/common_test.cpp
+++ test/cuBERT/common_test.cpp
@@ -2,15 +2,13 @@
 #include <mpi.h>
 TEST_F(CommonTest, memcpy) {
     MPI_Init(NULL,NULL);
-
-    int rank;
-    MPI_COMM_rank(MPI_COMM_WORLD, &rank);
-
+    MPI_Barrier(MPI_COMM_WORLD);
+    
     float *array = (float *) cuBERT::malloc(sizeof(float) * 10);
     float *array_b = (float *) cuBERT::malloc(sizeof(float) * 10);
+    MPI_Barrier(MPI_COMM_WORLD);
 
     cuBERT::memcpy(array, array_b, sizeof(float) * 10, 3);
-
     cuBERT::free(array_b);
     cuBERT::free(array);
 
